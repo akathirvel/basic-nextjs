f1c1db8253432602013550df26c68530f3805aa0
akathirvel <39790038+akathirvel@users.noreply.github.com>
2021-09-03 11:12:29 +0200

    Update TestComponent.js

--- components/generic/TestComponent.js
+++ components/generic/TestComponent.js
@@ -5,7 +5,7 @@ const TestComponent = ({blok}) =>{
     return (
         <form>
             <label>
-                {blok.label}
+                {blok.label} i am  updating here
                 <input type="text" name="name" />
             </label>
             <input type="submit" value="Submit" />
